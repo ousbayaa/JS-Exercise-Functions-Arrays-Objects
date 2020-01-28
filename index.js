98600a77a0a80d62bd2ff4bcfb57c6261381a43e
Ous Bayaa <ousbayaa@ous.local>
2020-01-27 22:39:09 -0800

    repaired 6 errors.

--- index.js
+++ index.js
@@ -39,8 +39,10 @@ function addNumbers(num1, num2) {
  *   email: "leia@leia.com",
  * }
 */
-function makePersonObject(/* code here */) {
+function makePersonObject(id, name, email) {
   /* code here */
+  var personObject = {id, name, email};
+  return personObject;
 }
 
 /**
@@ -56,8 +58,9 @@ function makePersonObject(/* code here */) {
  * passing { id: 1, name: 'Leia', email: 'leia@leia.com` } as the argument,
  * the returned value should look like `Hello, my name is Leia`.
 */
-function getName(/* code here */) {
+function getName(personObject) {
   /* code here */
+  return "Hello, my name is " + personObject.name;
 }
 
 /**
@@ -73,12 +76,19 @@ function getName(/* code here */) {
  *         and returns a string like `Hello, my name is {name}`.
  *         where `{name}` is the name passed into `makeSmartPerson`.
 */
-function makeSmartPerson(/* code here */) {
+function makeSmartPerson(name) {
   /* code here */
+  var someObj = {
+    name: name,
+    sum: function (num1, num2) {
+      return num1 + num2;
+    },
+    speak: function () {
+      return "Hello, my name is " + name;
+    }
+  };
+  return someObj;
 }
-
-
-
 
 
 /*
